ad1a4559f285d39658172f9e11f3b9eb2b6de448
ss1119 <y.sasaki111919@gmail.com>
2023-06-23 16:39:51 +0900

    fix

--- src/http/http3/server/demoserver.ts
+++ src/http/http3/server/demoserver.ts
@@ -25,8 +25,8 @@ const schemeName = undefined;
 const resourceList = undefined;
 let host = process.argv[2] || "0.0.0.0";
 let port = parseInt(process.argv[3]) || 4433;
-let key  = process.argv[4] || "../../../../../keys/selfsigned_default.key";
-let cert = process.argv[5] || "../../../../../keys/selfsigned_default.crt";
+let key  = "../../../../../keys/selfsigned_default.key";
+let cert = "../../../../../keys/selfsigned_default.crt";
 
 if (isNaN(Number(port))) {
     console.log("port must be a number: node ./main.js 127.0.0.1 4433 ca.key ca.cert");
@@ -36,8 +36,9 @@ if (isNaN(Number(port))) {
 Constants.LOG_FILE_NAME = "server.log";
 
 VerboseLogging.info("Running QUICker server at " + host + ":" + port + ", with certs: " + key + ", " + cert);
+VerboseLogging.info(__dirname);
 
-let server: Http3Server = new Http3Server(resolve(__dirname + key), resolve(__dirname + cert), "rr", resourceList);
+let server: Http3Server = new Http3Server(resolve(__dirname + "/" + key), resolve(__dirname + "/" + cert), "rr", resourceList);
 server.listen(port, host);
 
 console.log("HTTP/3 server listening on port "+ host +":"+ port +", log level " + Constants.LOG_LEVEL);
