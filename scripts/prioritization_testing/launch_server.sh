99bcfecd526e85fbf0a904286994b3dc44bb8617
ss1119 <y.sasaki111919@gmail.com>
2023-06-23 15:33:27 +0900

    fix script

--- scripts/prioritization_testing/launch_server.sh
+++ scripts/prioritization_testing/launch_server.sh
@@ -3,7 +3,4 @@
 # launch_server.sh PRIORITY_SCHEME TEST_CASE
 # e.g., launch_server.sh pmeenan syntheticwijnants
 cd ../../
-LOG_FILE_PATH="./out/http/http3/server/$2_$1_server_1.log"
-LOG_FILE_DIR=$(dirname "$LOG_FILE_PATH")
-mkdir -p "$LOG_FILE_DIR"
-LOG_LEVEL=debug DISABLE_STDOUT=false ../node/out/Release/node --max-old-space-size=8192 --inspect ./out/http/http3/server/demoserver.js $1 "${2}_$1_server_1" "$LOG_FILE_PATH" prioritization_testcases/$2 public/prioritization_testcases/$2/prioritization_resource_lists/resource_list.json
+LOG_LEVEL=debug DISABLE_STDOUT=false ../node/out/Release/node --max-old-space-size=8192 --inspect ./out/http/http3/server/demoserver.js $1 "$2_$1_server_1" "/$2_$1_server_1.log" "prioritization_testcases/$2" "public/prioritization_testcases/$2/prioritization_resource_lists/resource_list.json"
