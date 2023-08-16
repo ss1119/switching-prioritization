f1775164902aad5bf1f65ae0887114c2d5fbc651
ss1119 <y.sasaki111919@gmail.com>
2023-08-16 15:18:32 +0900

    make client sh

---
+++ scripts/server_config/control/start_client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "Need to pass in IP to listen on as first parameter. Usually: ./start_client.sh 0.0.0.0"
+	exit 1
+fi
+
+# typical way to call this: ./start_client.sh
+
+echo "First shutting down running containers (if they exist), then starting new quicker/quicker:latest container."
+
+sudo docker stop quicker_client && sudo docker rm quicker_client
+sudo docker run --privileged --restart unless-stopped --name quicker_client --network quicker --net=host -p 4433:4433/udp -p 4434:4434/udp -p 4435:4435/udp --volume=/Users/yudai/switching-prioritization-logs:/logs -d quicker/quicker:latest "$@"
+
+echo "Now run    sudo docker logs -f quicker_client    to view output, or    sudo docker exec -it quicker_client bash    to login to container"
